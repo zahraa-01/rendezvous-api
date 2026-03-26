3c164df5f051e5838e69c947699f9750fcf98ebf
zahraa-01 <zahraa.karim@booking.com>
2026-03-26 12:13:37 +0000

    Add Place model with migrations and passing basic tests

--- places/models.py
+++ places/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.URLField(blank=True, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- places/tests/test_models.py
+++ places/tests/test_models.py
@@ -1,6 +1,30 @@
 from django.test import TestCase
+from django.utils import timezone
+from places.models import Place
 
 
 class TestPlaceModel(TestCase):
-    def test_placeholder(self):
-        self.assertTrue(True)
+
+    def setUp(self):
+        self.place = Place.objects.create(
+            name='Café Lumière',
+            city='Paris',
+            country='France',
+            description='A cozy café near the Seine.',
+        )
+
+    def test_can_create_place(self):
+        self.assertEqual(self.place.name, 'Café Lumière')
+        self.assertEqual(self.place.city, 'Paris')
+        self.assertEqual(self.place.country, 'France')
+        self.assertEqual(self.place.description, 'A cozy café near the Seine.')
+
+    def test_image_defaults_to_empty_string(self):
+        self.assertEqual(self.place.image, '')
+
+    def test_created_at_is_auto_set(self):
+        self.assertIsNotNone(self.place.created_at)
+        self.assertLessEqual(self.place.created_at, timezone.now())
+
+    def test_str_returns_name(self):
+        self.assertEqual(str(self.place), 'Café Lumière')
