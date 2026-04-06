e843d919f38f91fb8ca486e86a7d091dfa098919
zahraa-01 <zahraa.karim@booking.com>
2026-04-07 00:32:36 +0100

    Add Cloudinary avatar upload with local fallback

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +9,7 @@ urlpatterns = [
     path('api/auth/', include('authentication.urls')),
     path('api/profile/', include('profiles.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
