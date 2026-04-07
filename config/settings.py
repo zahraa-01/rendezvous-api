958ab9afb5055ee3f46baeb67e10e8c821a5e93b
zahraa-01 <zahraa.karim@booking.com>
2026-04-07 22:04:25 +0100

    Add STATIC_ROOT for production deployment

--- config/settings.py
+++ config/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media / Cloudinary storage config
 
